0723060e4be4fcb76a6c923e4f72a6139a9d1e1c
siamcodes <siamcodes@gmail.com>
2021-08-08 11:38:50 +0700

    initial

---
+++ components/Slide.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Slide extends Component {
+    render() {
+        return (
+            <div>
+                Slide
+            </div>
+        )
+    }
+}
